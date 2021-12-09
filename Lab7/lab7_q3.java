825bef758ca2b982d86688c1e9abe3f3e2d23ee2
Tejaswini-Gurumoorthy <75904755+Tejaswini-Gurumoorthy@users.noreply.github.com>
2021-12-09 11:01:11 +0530

    Create lab7_q3.java

---
+++ Lab7/lab7_q3.java
@@ -0,0 +1,13 @@
+public class lab7_q3
+{
+    public static void main(String args[])
+    {
+        int i= 2;
+        Integer iobj= i;
+        System.out.println(iobj);
+        iobj=3;
+        int j= iobj;
+        System.out.println(j);
+
+    }
+}
