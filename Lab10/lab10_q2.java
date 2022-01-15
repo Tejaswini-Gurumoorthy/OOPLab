131b8ef6b69f3dcc4afab3095d9167cf66a2f0a7
Tejaswini-Gurumoorthy <75904755+Tejaswini-Gurumoorthy@users.noreply.github.com>
2022-01-15 21:04:02 +0530

    Update lab10_q2.java

--- Lab10/lab10_q2.java
+++ Lab10/lab10_q2.java
@@ -11,25 +11,30 @@ public class lab10_q2
 {
     public static void main(String args[])
     {
+        System.out.println("NAME : TEJASWINI GURUMOORTHY");
+        System.out.println("REG NO. : 20091102");
         Scanner sc= new Scanner(System.in);
         int size;
-        System.out.println("Enter the size of array : ");
-        size= sc.nextInt();
-        if(size<0)
-        {
-            throw new NegativeArraySizeException("Size of array cannot be negative !!!");
+        try {
+            System.out.println("Enter the size of array : ");
+            size = sc.nextInt();
+            if (size < 0) {
+                throw new NegativeArray("Size of array cannot be negative !!!");
+            }
+            int arr[] = new int[size];
+            System.out.println("Enter array elements : ");
+            for (int i = 0; i < size; i++) {
+                arr[i] = sc.nextInt();
+            }
+            System.out.println("The elements of array are : ");
+            for (int i = 0; i < size; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
         }
-        int arr[]=new int[size];
-        System.out.println("Enter array elements : ");
-        for (int i=0;i<size;i++)
+        catch (NegativeArray n)
         {
-           arr[i]=sc.nextInt();
+            System.out.println("Error : "+n.getMessage());
         }
-        System.out.println("The elements of array are : ");
-        for (int i=0;i<size;i++)
-        {
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println();
     }
 }
