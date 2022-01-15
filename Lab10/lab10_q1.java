a2e3e7a847941ce7cf6df0098166b407820e7627
Tejaswini-Gurumoorthy <75904755+Tejaswini-Gurumoorthy@users.noreply.github.com>
2022-01-15 20:55:48 +0530

    Update lab10_q1.java

--- Lab10/lab10_q1.java
+++ Lab10/lab10_q1.java
@@ -1,40 +1,44 @@
 import java.util.Scanner;
 import java.util.*;
-public class FloatException extends Exception
+class FloatException extends Exception
 {
     public FloatException(String s)
     {
         super(s);
     }
 }
-
-
 public class Input_Exception {
     public static void main(String args[])
     {
+        System.out.println("TEJASWINI GURUMOORTHY");
+        System.out.println("REG NO. : 200911202");
         Scanner sc= new Scanner(System.in);
         int sum=0;
-        int num;
+        float num;
         while (true)
         {
             try {
                 System.out.println("Enter number : ");
-                num= sc.nextInt();
+                num = sc.nextFloat();
 
-                if(num==-1)
-                {
-                    System.out.println(sum);
+                int n = (int) num;
+                if (num - n != 0) {
+                    throw new FloatException("Float numbers invalid !!");
+                }
+
+                if (num == -1) {
+                    System.out.println("Sum is : "+ sum);
                     break;
 
                 }
-                sum+=num;
-
+                sum += num;
             }
-            catch (FloatException e)
+            catch (FloatException f)
             {
-                
-
+                System.out.println("Error : "+f.getMessage());
             }
+
+
 
 
 
